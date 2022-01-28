759a2d107050e80295b6eee4539de10cc2c07c06
Haydenn Harper <haydennharper@gmail.com>
2022-01-28 00:53:05 -0500

    Layout complete. Light and Dark Mode created

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom"
 import Home from "./home.jsx";
+// import Dark from "../stylesDark.css";
+import Light from "../styles.css";
+
 
 export default function App() {
+
   return (
     <div className = "App">
       <Home/>
--- webpack.config.js
+++ webpack.config.js
@@ -10,16 +10,21 @@ module.exports = {
   devtool: 'eval-source-map', // add devtool option
   module: {
     rules: [{
-      test: /\.(js|jsx)$/, //regex to test for .js or .jsx file endings
-      exclude: /node_modules/, //regex that specifies node_modules folder
-      use: {
-        //Loader to use on files specified by 'test' above
-        loader: 'babel-loader',
-        // Additional options specific to the loader
-        options: {
-          presets: ['@babel/preset-env', '@babel/preset-react']
+        test: /\.(js|jsx)$/, //regex to test for .js or .jsx file endings
+        exclude: /node_modules/, //regex that specifies node_modules folder
+        use: {
+          //Loader to use on files specified by 'test' above
+          loader: 'babel-loader',
+          // Additional options specific to the loader
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
         }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       }
-    }]
+    ]
   }
 };
