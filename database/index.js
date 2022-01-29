54771e61246fc2154c3f65e410007146f7d1dcca
Andrew Carnero <andrew.carnero@gmail.com>
2022-01-29 16:50:51 -0500

    build Canvas and User schemas

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/Book_Nook')
+mongoose.connect('mongodb://ec2-54-152-31-241.compute-1.amazonaws.com:27017/Book_Nook')
   .then(() => console.log('Book_Nook database connected'))
   .catch((error) => console.log(error));
 
@@ -10,19 +10,46 @@ const authenticateSchema = new mongoose.Schema({
   password: { type: String, required: true },
 })
 
-// TODO: create User schema
+const canvasSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  comment: { type: String, min: 10, max: 500, required: true},
+}, {timestamps: true});
+
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  userbook: {},
-  friends: [],
-  canvas: [],
-  settings: {},
-})
+  userBooks: [{
+    userBookId: String, //googleapi book ID
+    title: { type: String, required: true, unique: true },
+    author: { type: String, required: true, unique: true },
+    clubbed: {
+        status: { type: Boolean, default: false },
+        date: { type: String, default: null },
+      },
+    current: {
+        status: { type: Boolean, default: false },
+        date: { type: String, default: null },
+      },
+    past: {
+        status: { type: Boolean, default: false },
+        date: { type: String, default: null },
+      },
+    queued: {
+        status: { type: Boolean, default: false },
+        date: { type: String, default: null },
+      },
+    }, {timestamps: true} ],
+  friends: [{ _id: false, username: String }],
+  settings: {
+    theme: { type: String },
+   },
+}, {timestamps: true} );
 
 const Authenticate = mongoose.model('Authenticate', authenticateSchema);
 const User = mongoose.model('User', userSchema);
+const Canvas = mongoose.model('Canvas', canvasSchema);
 
 module.exports = {
   Authenticate,
-  User
+  User,
+  Canvas
 }
