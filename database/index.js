eca5224d7a4a2cb5f81cc6102a2e3d528810b181
bryan <brynrmrzz@outlook.com>
2022-01-29 15:15:47 -0500

    Added comma

--- database/index.js
+++ database/index.js
@@ -11,19 +11,22 @@ const authenticateSchema = new mongoose.Schema({
 })
 
 const bookDataSchema = new mongoose.Schema({
-  lookupId: {type: String},
+  lookup_id: {type: String},
   primary_isbn: {type: Number},
   reviews: [{
     username: {type: String},
-    reviewDate: {type: Date},
+    review_date: {type: Date, default: Date.now},
     rating: {type: Number, min: 1, max: 5},
-    reviewBody: {type: String, min: 25, max: 1000},
-    report: {type: Boolean},
-    helpful: {type: Number},
+    review_body: {type: String, min: 25, max: 1000},
+    reported_review: {type: Boolean},
+    helpful_review: {type: Number},
     comments: [{
-        commenter: {type: String},
-        time: {type: Date},
-        comment: {type: String},
+        comment_id: new mongoose.ObjectId,
+        commenter: { type: String},
+        comment_time: { type: Date , default: Date.now},
+        comment_body: { type: String, min: 10, max: 10000},
+        reported_comment: { type: Boolean },
+        helpful_comment: { type: Number },
       }],
   }],
 })
