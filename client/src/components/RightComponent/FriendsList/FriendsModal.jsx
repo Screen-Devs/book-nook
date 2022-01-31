6630d3607fa682e970e94854450c622d10dbd1bc
maraquepo <maraquepo@gmail.com>
2022-01-30 18:24:46 -0600

    functioning remove friend button

--- client/src/components/RightComponent/FriendsList/FriendsModal.jsx
+++ client/src/components/RightComponent/FriendsList/FriendsModal.jsx
@@ -1,4 +1,14 @@
-import { Avatar, Box, IconButton, List, ListItem, ListItemAvatar, ListItemText } from '@material-ui/core';
+import {
+  Avatar,
+  Box,
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemSecondaryAction,
+  ListItemText,
+  Paper
+} from '@material-ui/core';
 import React from 'react';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import DeleteIcon from '@material-ui/icons/Delete';
@@ -10,52 +20,50 @@ const styleM = {
   transform: 'translate(-50%, -50%)',
   width: 500,
   height: 500,
-  bgcolor: 'background.paper',
+  background: 'white',
   border: '0.5px solid lightgrey',
   boxShadow: 24,
   p: 4,
   overflowY: 'scroll',
   display: 'flex',
   flexDirection: 'column',
-  alignItems: 'center'
 };
 
-const FriendsModal = ({friendsList, set, remove}) => {
+const FriendsModal = ({ friendsList, set, remove }) => {
 
   const handleRemove = (ID) => {
-    const newFriends = friendsList.filter((friend) => friend.ID !== ID)
-    set(newFriends)
-  }
+    const newFriends = friendsList.filter((friend) => friend.ID !== ID);
+    set(newFriends);
+  };
+
+  console.log(friendsList)
 
   return (
+    <Paper>
       <Box style={styleM}>
-        <div style={{alignSelf: 'center'}}>Friend List</div>
+        <div style={{ alignSelf: 'center' }}>Friend List</div>
         <List>
           {friendsList.map((datum) => {
             return (
-              <ListItem
-                key={datum.ID}
-                secondaryAction={
-                  <IconButton edge='end' onClick={()=> handleRemove(datum.ID)}>
-                    <DeleteIcon/>
-                  </IconButton>
-                }
-                style={{height: 60, width: 400}}
-              >
+              <ListItem key={datum.ID} style={{ height: 60, width: 400 }}>
                 <ListItemAvatar>
                   <Avatar>
-                    <AccountCircleIcon/>
+                    <AccountCircleIcon />
                   </Avatar>
                 </ListItemAvatar>
-                <ListItemText
-                  primary={datum.FirstNameLastName}
-                  />
+                <ListItemText primary={datum.FirstNameLastName} />
+                <ListItemSecondaryAction>
+                  <IconButton edge='end' onClick={() => handleRemove(datum.ID)}>
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
               </ListItem>
-            )
+            );
           })}
         </List>
       </Box>
-  )
+    </Paper>
+  );
 };
 
 export default FriendsModal;
