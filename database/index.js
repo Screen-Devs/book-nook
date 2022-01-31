56a821db16b425fe211752a646ceebacc8e8c4a6
Haydenn Harper <haydennharper@gmail.com>
2022-01-31 17:25:48 -0500

    weird bugs fixed

--- database/index.js
+++ database/index.js
@@ -18,55 +18,131 @@ const authenticateSchema = new mongoose.Schema({
 });
 
 const userBookSchema = new mongoose.Schema({
-  gBookId: { type: String, unique: true}, //googleapi book ID
-  title: { type: String, unique: false },
-  authors: { type: Array, unique: false },
+  gBookId: {
+    type: String,
+    unique: true
+  }, //googleapi book ID
+  title: {
+    type: String,
+    unique: false
+  },
+  authors: {
+    type: Array,
+    unique: false
+  },
   clubbed: {
-      status: { type: Boolean, default: false },
-      date: { type: String, default: null },
+    status: {
+      type: Boolean,
+      default: false
     },
+    date: {
+      type: String,
+      default: null
+    },
+  },
   current: {
-      status: { type: Boolean, default: false },
-      date: { type: String, default: null },
+    status: {
+      type: Boolean,
+      default: false
     },
+    date: {
+      type: String,
+      default: null
+    },
+  },
   past: {
-      status: { type: Boolean, default: false },
-      date: { type: String, default: null },
+    status: {
+      type: Boolean,
+      default: false
     },
+    date: {
+      type: String,
+      default: null
+    },
+  },
   queued: {
-      status: { type: Boolean, default: false },
-      date: { type: String, default: null },
+    status: {
+      type: Boolean,
+      default: false
     },
+    date: {
+      type: String,
+      default: null
+    },
+  },
 });
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
   userBooks: [userBookSchema],
-  friends: [{ _id: false, username: String }],
+  friends: [{
+    _id: false,
+    username: String
+  }],
   canvas: Array,
   settings: {
-    theme: { type: String },
-   },
-}, {timestamps: true} );
+    theme: {
+      type: String
+    },
+  },
+}, {
+  timestamps: true
+});
 
 const bookDataSchema = new mongoose.Schema({
-  lookup_id: {type: String},
+  lookup_id: {
+    type: String
+  },
   reviews: [{
-    review_id: mongoose.ObjectId,,
-    username: {type: String},
-    review_date: {type: Date, default: Date.now},
-    rating: {type: Number, min: 1, max: 5},
-    review_body: {type: String, min: 25, max: 1000},
-    reported_review: {type: Boolean},
-    helpful_review: {type: Number},
+    review_id: mongoose.ObjectId,
+    username: {
+      type: String
+    },
+    review_date: {
+      type: Date,
+      default: Date.now
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5
+    },
+    review_body: {
+      type: String,
+      min: 25,
+      max: 1000
+    },
+    reported_review: {
+      type: Boolean
+    },
+    helpful_review: {
+      type: Number
+    },
     comments: [{
-        comment_id: mongoose.ObjectId,
-        commenter: { type: String},
-        comment_time: { type: Date , default: Date.now},
-        comment_body: { type: String, min: 10, max: 1000},
-        reported_comment: { type: Boolean },
-        helpful_comment: { type: Number },
-      }],
+      comment_id: mongoose.ObjectId,
+      commenter: {
+        type: String
+      },
+      comment_time: {
+        type: Date,
+        default: Date.now
+      },
+      comment_body: {
+        type: String,
+        min: 10,
+        max: 1000
+      },
+      reported_comment: {
+        type: Boolean
+      },
+      helpful_comment: {
+        type: Number
+      },
+    }],
   }],
 })
 
