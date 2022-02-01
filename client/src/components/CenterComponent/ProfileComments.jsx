1d62a6aa02164fb974498d76d99b5758c18c3730
Haydenn Harper <haydennharper@gmail.com>
2022-02-01 17:01:36 -0500

    Form is built for write in book, working on pulling out values

--- client/src/components/CenterComponent/ProfileComments.jsx
+++ client/src/components/CenterComponent/ProfileComments.jsx
@@ -2,13 +2,26 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import 'animate.css';
 import CommentModule from '../CommentModule.jsx';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 
 export default function ProfileComments ({ userData }) {
   const [canvas, setCanvasList] = useState([]);
 
+  const [commentText, setCommentText] = useState(),
+  onInput = ({target:{commentText}}) => setCommentText(commentText),
+  onFormSubmit = e => {
+    e.preventDefault()
+    console.log(commentText)
+    setCommentText()
+  }
+
   useEffect(() => {
     setCanvasList(userData[0].canvas);
   }, userData)
+
+
 
   //TODO: Need to implement a way to add a comment
 
@@ -16,7 +29,22 @@ export default function ProfileComments ({ userData }) {
     <>
       <div className = "centerComponent" >
       <div className="userDetails animate__animated animate__flipInX"><h5>User: {userData[0].username}</h5></div>
-      <div className="writeMe animate__animated animate__flipInX"><h5>Write in my book...</h5></div>
+
+
+      <div className="writeMe animate__animated animate__flipInX">
+        <h5 style={{fontWeight: 'bold',}}>Write in my book...</h5>
+
+        <div style={{alignSelf: 'center'}}>
+          <Form onSubmit={onFormSubmit}>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+              <Form.Control className="writeMeTextArea" as="textarea" onChange={onInput} value={commentText} placeholder="scribble scribble scribble..." rows={5} />
+            </Form.Group>
+          </Form>
+      <Button className="publishButton" type="submit" variant="dark" style={{width: '100px', marginLeft: '230px'}}>Publish</Button>
+        </div>
+        </div>
+
+
       <div className="userBook animate__animated animate__flipInY">
         {canvas.length === 0 ? <CommentModule body="No comments :(" /> :
         canvas.map((comment) => {
