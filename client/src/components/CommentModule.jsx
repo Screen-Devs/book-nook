16117ee3f16c92a87aaf5795cdab7b45d3040de7
bryan <brynrmrzz@outlook.com>
2022-02-02 17:08:49 -0500

    working on canvas

--- client/src/components/CommentModule.jsx
+++ client/src/components/CommentModule.jsx
@@ -4,18 +4,20 @@ import 'animate.css';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
-export default function CommentModule({ username = 'Find a friend!', body, time }) {
+export default function CommentModule({ comment }) {
+  console.log('Comment module ', comment)
+
   return (
     <div className = "commentModule" >
       <Card className="text-center commentModuleBody">
-        <Card.Header><b>{username}</b></Card.Header>
+        <Card.Header>{comment.username}</Card.Header>
         <Card.Body>
           <Card.Text>
-            {body}
+            {comment.message}
           </Card.Text>
-          <Button variant="dark">Button</Button>
+          {/* <Button variant="dark">Button</Button> */}
         </Card.Body>
-        <Card.Footer className="text-muted"><i>2 days ago</i></Card.Footer>
+        <Card.Footer className="text-muted"><i>{comment.date}</i></Card.Footer>
       </Card>
     </div >
   );
