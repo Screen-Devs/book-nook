30083adbbb49c1b0e9bf41e288283fbb545d33c5
maraquepo <maraquepo@gmail.com>
2022-02-03 15:16:05 -0600

    added carousel to suggested books

---
+++ client/src/components/RightComponent/SuggestedBooks/SuggstedBooks.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import styled from 'styled-components';
+import SuggestedCarousel from './SuggestedCarousel.jsx';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Title = styled.div`
+
+`;
+
+const Content = styled.div`
+  width: 100%;
+`;
+
+const SuggstedBooks = () => {
+  return (
+    <Wrapper>
+      <Title>
+        <Button
+          className='sideComponentTitle'
+          style={{ display: 'flex', justifyContent: 'center' }}
+          variant='dark'
+        >
+          Suggested Books
+        </Button>
+      </Title>
+      <Content>
+        <SuggestedCarousel/>
+      </Content>
+    </Wrapper>
+  );
+};
+
+export default SuggstedBooks;
