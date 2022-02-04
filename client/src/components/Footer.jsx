3d6e7530ab34565e46acb9570f035bacebb48eb9
maraquepo <maraquepo@gmail.com>
2022-02-04 12:57:50 -0600

    added footer with our linkedin profiles linked

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,57 @@
+import { Card, Link } from "@material-ui/core";
 import React from "react";
 import ReactDOM from "react-dom"
 // import "/Users/haydennharper/HackReactor/Blue/book-nook/client/src/images/mediumLogo.png"
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  font-size: 15px;
+  color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 2px 8px;
+`
+const devs = [
+  {
+    name: 'S. Beddow,',
+    linkedIn: "https://www.linkedin.com/in/samuel-r-r-beddow/"
+  },
+  {
+    name: 'A. Carnero,',
+    linkedIn: "https://www.linkedin.com/in/andrew-carnero/"
+  },
+  {
+    name: 'H. Harper,',
+    linkedIn: "https://www.linkedin.com/in/haydenn-harper/"
+  },
+  {
+    name: 'B. Ramirez,',
+    linkedIn: "https://www.linkedin.com/in/brynrmrz/"
+  },
+  {
+    name: 'M. Raquepo,',
+    linkedIn: "https://www.linkedin.com/in/matthew-raquepo/"
+  },
+  {
+    name: 'K. Sheng',
+    linkedIn: "https://www.linkedin.com/in/kevin-c-sheng/"
+  }
+        ]
 
 export default function Footer() {
+
   return (
     <div className = "footer">
-      Footer
+      <Card>
+      <Wrapper>
+        Developed by
+        {devs.map((dev) => {
+          console.log(dev.linkedIn)
+          return <a target="_blank" style={{padding: 3, textDecoration: 'none'}} href={dev.linkedIn}>{dev.name}</a>
+        })}
+      </Wrapper>
+      </Card>
     </div>
   );
 }
